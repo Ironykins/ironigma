8d15734c93ea11688cb8b162d6419b6246000088
Ironykins <konrad.aust@gmail.com>
2015-07-27 00:13:10 -0600

    Fixed main page.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 angular.module('ironigma', [])
 .controller('enigma', ['$scope', function($scope) {
-    $scope.enigma = new M4();
+    $scope.enigma = new M3();
     $scope.enigma.rotors[0].step();
 }])
 
