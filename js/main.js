1f617128335f45830a10d6704306fd20f5bfb6ed
Ironykins <konrad.aust@gmail.com>
2015-07-30 19:13:24 -0600

    Added some more basic controls to the front end.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@
 angular.module('ironigma', [])
 .controller('enigma', ['$scope', function($scope) {
     $scope.enigma = new M3();
+    $scope.ciphertext = "";
 
     $scope.step = function() { $scope.enigma.step() }
+    $scope.reset = function() { $scope.enigma.reset() }
+
+    $scope.charEncrypt = function() { 
+        $scope.ciphertext += $scope.enigma.encrypt($scope.input);
+        $scope.input = "";
+
+    }
 }])
 
